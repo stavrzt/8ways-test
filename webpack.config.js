281aae2f1e0fb642eb6573fbccdf844d668bc4d1
y.mudryi <y.mudryi@zinitsolutions.com>
2017-11-02 15:18:21 +0200

    fixed styles in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         ]
     },
     plugins: [
-        new ExtractTextPlugin("../css/0styles.css"),
+        new ExtractTextPlugin("../css/styles.css"),
         new WatchLiveReloadPlugin({
             files: [
                 './**/*.html',
